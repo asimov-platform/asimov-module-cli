c48bcfb3490e57cc63dbe3fa595a723fec5de342
Arto Bendiken <arto@asimov.so>
2025-04-13 09:59:03 +0200

    Implement `asimov module install`.

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,13 +1,74 @@
 // This is free and unencumbered software released into the public domain.
 
-use crate::{registry, StandardOptions, SysexitsError};
+use crate::{
+    registry::{self, ModuleMetadata},
+    StandardOptions, SysexitsError,
+};
+use std::{io::ErrorKind, process::Command};
 
-pub fn install(module_names: Vec<String>, _flags: &StandardOptions) -> Result<(), SysexitsError> {
+#[tokio::main]
+pub async fn install(
+    module_names: Vec<String>,
+    _flags: &StandardOptions,
+) -> Result<(), SysexitsError> {
+    let mut modules_to_install: Vec<ModuleMetadata> = vec![];
+
     for module_name in module_names {
         if registry::is_installed(&module_name) {
             continue; // skip already installed modules
         }
-        // TODO
+
+        match registry::fetch_module(&module_name).await {
+            Some(module) => {
+                modules_to_install.push(module.clone());
+            }
+            None => {
+                eprintln!("unknown module: {}", module_name);
+                return Err(SysexitsError::EX_UNAVAILABLE);
+            }
+        }
     }
+
+    for module in modules_to_install {
+        use registry::ModuleType::*;
+        let package_name = format!("asimov-{}-module", module.name);
+
+        let result = match module.r#type {
+            Rust => Command::new("cargo")
+                .args(["install", &package_name])
+                .status(),
+            Ruby => Command::new("gem")
+                .args(["install", &package_name])
+                .status(),
+            Python => Command::new("pip3")
+                .args(["install", &package_name])
+                .status(),
+        };
+
+        match result {
+            Err(error) if error.kind() == ErrorKind::NotFound => {
+                eprintln!(
+                    "failed to install module `{}`: missing {} environment",
+                    module.name,
+                    module.r#type.to_string()
+                );
+                return Err(SysexitsError::EX_UNAVAILABLE);
+            }
+            Err(error) => {
+                eprintln!("failed to install module `{}`: {}", module.name, error);
+                return Err(SysexitsError::EX_OSERR);
+            }
+            Ok(status) if !status.success() => {
+                eprintln!(
+                    "failed to install module `{}`: exit code {}",
+                    module.name,
+                    status.code().unwrap_or_default()
+                );
+                return Err(SysexitsError::EX_SOFTWARE);
+            }
+            Ok(_) => {}
+        }
+    }
+
     Ok(())
 }
--- src/commands/uninstall.rs
+++ src/commands/uninstall.rs
@@ -3,11 +3,18 @@
 use crate::{registry, StandardOptions, SysexitsError};
 
 pub fn uninstall(module_names: Vec<String>, _flags: &StandardOptions) -> Result<(), SysexitsError> {
+    let mut modules_to_uninstall: Vec<String> = vec![];
+
     for module_name in module_names {
         if !registry::is_installed(&module_name) {
             continue; // skip not installed modules
         }
+        modules_to_uninstall.push(module_name.clone());
+    }
+
+    for _module_name in modules_to_uninstall {
         // TODO
     }
+
     Ok(())
 }
