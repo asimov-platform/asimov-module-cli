98927c63f5d1b69a9d29afe04103d13149d470cc
Samuel Sarle <samuel@sarle.com>
2025-06-06 17:08:09 +0300

    Use `asimov-apify-module` in example/test

--- tests/install_github_release.rs
+++ tests/install_github_release.rs
@@ -5,10 +5,10 @@ use asimov_module_cli::registry::{ModuleMetadata, ModuleType};
 #[tokio::test]
 async fn main() {
     let md = ModuleMetadata {
-        name: "jq".into(),
-        version: "0.1.2".into(),
+        name: "apify".into(),
+        version: "0.1.1".into(),
         r#type: ModuleType::Rust,
-        url: "https://github.com/asimov-modules/asimov-jq-module".into(), // does not matter, it's not used
+        url: "https://github.com/asimov-modules/asimov-apify-module".into(), // does not matter, it's not used
     };
 
     let res = asimov_module_cli::registry::github::install_from_github(&md, 2)
