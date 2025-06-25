ecb21084bae0e6a30ba4ee240110757bbcbac168
Samuel Sarle <samuel@asimov.systems>
2025-06-25 12:28:45 +0300

    Show installed binary modules in `list`. (#8)

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,10 +1,43 @@
 // This is free and unencumbered software released into the public domain.
 
-use crate::{registry, StandardOptions, SysexitsError};
-use color_print::{cprint, cprintln};
+use crate::{StandardOptions, SysexitsError, registry};
+use asimov_env::paths::asimov_root;
+use color_print::{ceprintln, cprint, cprintln};
 
 #[tokio::main]
 pub async fn list(flags: &StandardOptions) -> Result<(), SysexitsError> {
+    let module_dir_path = asimov_root().join("modules");
+
+    let md = tokio::fs::metadata(&module_dir_path).await;
+    if md.is_ok_and(|md| md.is_dir()) {
+        let mut module_dir = tokio::fs::read_dir(module_dir_path)
+            .await
+            .inspect_err(|e| {
+                ceprintln!("<s,r>error:</> failed to read module manifest directory: {e}")
+            })?;
+        loop {
+            match module_dir.next_entry().await {
+                Ok(None) => break,
+                Ok(Some(entry)) => {
+                    let Some(name) = entry
+                        .path()
+                        .file_stem()
+                        .map(|stem| stem.to_string_lossy().to_string())
+                    else {
+                        continue;
+                    };
+
+                    if flags.verbose > 0 {
+                        cprintln!("<s,g>✓</> {}\t\t", name);
+                    } else {
+                        cprintln!("<s,g>✓</> {}", name);
+                    }
+                }
+                Err(e) => continue,
+            }
+        }
+    }
+
     for module in registry::fetch_modules().await? {
         let is_installed = module.is_installed()?;
         if is_installed {
@@ -13,9 +46,9 @@ pub async fn list(flags: &StandardOptions) -> Result<(), SysexitsError> {
             cprint!("<s><r>✗</></> ");
         }
         if flags.verbose > 0 {
-            println!("{}\t{}\t{}", module.name, module.version, module.r#type);
+            cprintln!("{}\t{}\t{}", module.name, module.version, module.r#type);
         } else {
-            cprintln!("{}", module.name,);
+            cprintln!("{}", module.name);
         }
     }
     Ok(())
