f03aa667ba25b28b82141e47671e65f3d0659047
Samuel Sarle <samuel@asimov.systems>
2025-07-02 17:52:53 +0300

    Support the `link` and `browse` commands. (#12)

--- src/commands/link.rs
+++ src/commands/link.rs
@@ -5,6 +5,9 @@ use crate::{
     SysexitsError::{self, *},
     registry,
 };
+use asimov_env::paths::asimov_root;
+use asimov_module::models::ModuleManifest;
+use color_print::ceprintln;
 
 #[tokio::main]
 pub async fn link(
@@ -12,14 +15,36 @@ pub async fn link(
     _flags: &StandardOptions,
 ) -> Result<(), SysexitsError> {
     let module_name = module_name.as_ref();
-    match registry::fetch_module(module_name).await {
-        Some(module) => {
-            println!("{}", module.url);
-            Ok(())
+
+    let manifest_path = asimov_root()
+        .join("modules")
+        .join(format!("{module_name}.yaml"));
+
+    // try installed modules
+    if let Ok(manifest) = tokio::fs::read(&manifest_path).await.inspect_err(|e| {
+        tracing::warn!("install not found for `{module_name}`, failed to read manifest: {e}")
+    }) {
+        let manifest: ModuleManifest = serde_yml::from_slice(&manifest).map_err(|e| {
+            tracing::error!("failed parse manifest for module `{module_name}`: {e}");
+            EX_UNAVAILABLE
+        })?;
+
+        let mut links = manifest.links;
+        crate::sort_links(&manifest.name, &mut links);
+
+        for link in links {
+            println!("{link}");
         }
-        None => {
-            eprintln!("unknown module: {}", module_name);
-            Err(EX_UNAVAILABLE)
-        }
+
+        return Ok(());
     }
+
+    // try modules in registries
+    if let Some(module) = registry::fetch_module(module_name).await {
+        println!("{}", module.url);
+        return Ok(());
+    };
+
+    eprintln!("unknown module: {}", module_name);
+    Err(EX_UNAVAILABLE)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,52 @@ pub mod options {}
 pub mod registry;
 
 use clientele::{StandardOptions, SysexitsError};
+
+/// Sorts links from a module's manifest in the order that we'd like to display
+/// them for the command `link` and for choosing the URL to open for the command
+/// `browse`.
+pub(crate) fn sort_links(module_name: &str, links: &mut [impl AsRef<str>]) {
+    use std::cmp::Reverse;
+
+    links.sort_by_cached_key(|link| {
+        let Ok(url) = reqwest::Url::parse(link.as_ref()) else {
+            // it's not even a valid url? put it last
+            return Reverse(0);
+        };
+
+        let Some(host) = url.host_str() else {
+            // it doesn't have a host, put it last
+            return Reverse(0);
+        };
+
+        // give highest priority to github links under our org
+        let our_module = link.as_ref().contains("github.com/asimov-modules/") as i8;
+
+        let host_score =
+            // give priority to github links
+            (host.ends_with("github.com") as i8 * 2)
+            // then any of the package indices
+            + ((host.ends_with("crates.io") ||
+                host.ends_with("pypi.org") ||
+                host.ends_with("rubygems.org") ||
+                host.ends_with("npmjs.com")) as i8);
+
+        let path_score = {
+            let path = url.path();
+            // give highest priority to links which contain the exact module name
+            (path.contains(&format!("asimov-{module_name}-module")) as i8 * 3)
+            // next to links which contain `asimov-`
+            + (((path.contains("asimov-")
+                // and `-module`
+                && path.contains("-module")
+                // but not `/asimov-modules/`
+                && !path.contains("/asimov-modules/")) as i8) * 2)
+            // and finally if the path does contain `/asimov-modules/`
+            + (path.contains("/asimov-modules/") as i8)
+        };
+
+        // add all the scores together, then reverse it because we want the highest scores first (sort is ascending order)
+        // (add 1 to differentiate from the invalid/host-less links that we return early for)
+        Reverse(our_module * 5 + host_score + path_score + 1)
+    });
+}
